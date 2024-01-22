9baa4ebe8351eb8b6ab8df91f777471d33a8b690
DC Baker <dylan@greathat.ca>
2024-01-21 16:33:08 -0800

    Added a simplified required-text-box control to make validation easier

---
+++ Wildfire ICS Assist/CustomControls/TextBoxRequiredControl.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Wildfire_ICS_Assist.CustomControls
+{
+    partial class TextBoxRequiredControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(150, 150);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // TextBoxRequiredControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Name = "TextBoxRequiredControl";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Wildfire ICS Assist/CustomControls/TextBoxRequiredControl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wildfire_ICS_Assist.CustomControls
+{
+    public partial class TextBoxRequiredControl : UserControl
+    {
+        [Description("Determine whether the control shows multiple lines of text"), Category("Appearance")]
+        public bool Multiline
+        {
+            get => textBox1.Multiline; set
+            {
+                textBox1.Multiline = value;
+                if (value) { textBox1.ScrollBars = ScrollBars.Vertical; }
+                else { textBox1.ScrollBars = ScrollBars.None; }
+            }
+        }
+
+        public bool IsValid { get { return !string.IsNullOrEmpty(textBox1.Text); } }
+        public new string Text { get => textBox1.Text; }
+        public void SetText(string txt) { textBox1.Text = txt; }
+
+        public TextBoxRequiredControl()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            validateTextBox();
+        }
+
+        private void validateTextBox()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                errorProvider1.SetError(textBox1, "This field is required");
+                textBox1.BackColor = Program.ErrorColor;
+            }
+            else
+            {
+                errorProvider1.SetError(textBox1, string.Empty);
+                textBox1.BackColor = Program.GoodColor;
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            validateTextBox();
+        }
+
+        private void TextBoxRequiredControl_Load(object sender, EventArgs e)
+        {
+            validateTextBox();
+
+        }
+    }
+}
