0a48d3991bec742d93586109df027eb718c10eda
DC Baker <dylan@greathat.ca>
2024-10-28 12:51:58 -0700

    added a reference to the logging service

--- Wildfire ICS Assist/InternetSyncForms/InternetSyncStartForm.cs
+++ Wildfire ICS Assist/InternetSyncForms/InternetSyncStartForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WildfireICSDesktopServices;
+using WildfireICSDesktopServices.Logging;
 
 namespace Wildfire_ICS_Assist.InternetSyncForms
 {
