408d47c2dab857df18da1958e4ab4f41cd417e22
DC Baker <dylan@greathat.ca>
2024-11-06 09:10:10 -0800

    Added a screen to create new operational periods, work is in progress

---
+++ Wildfire ICS Assist/OperationalPeriodForms/CreateOperationalPeriodForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wildfire_ICS_Assist.OperationalPeriodForms
+{
+    public partial class CreateOperationalPeriodForm : BaseForm
+    {
+        public CreateOperationalPeriodForm()
+        {
+            InitializeComponent(); SetControlColors(this.Controls);
+        }
+    }
+}
