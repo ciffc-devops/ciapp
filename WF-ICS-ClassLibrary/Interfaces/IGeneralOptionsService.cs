30b5a92d53c261167a57eab813d644077d9c5194
DC Baker <dylan@greathat.ca>
2025-10-23 09:39:20 -0700

    Added form set

--- WF-ICS-ClassLibrary/Interfaces/IGeneralOptionsService.cs
+++ WF-ICS-ClassLibrary/Interfaces/IGeneralOptionsService.cs
@@ -20,6 +20,7 @@ namespace WF_ICS_ClassLibrary.Interfaces
         bool GetOptionsBoolValue(string ValueName);
         Guid GetGuidOptionValue(string ValueName);
         string GetStringOptionValue(string ValueName);
+        int GetIntOptionsValue(string ValueName);
         void UpsertOptionValue(object newValue, string property_name = null);
         void RemoveOptionValue(object removeValue, string property_name);
 
